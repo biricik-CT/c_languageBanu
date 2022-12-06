1f938b8e575c56225cec85d0ea3149db0c3db0d9
biricik-CT <71524881+biricik-CT@users.noreply.github.com>
2022-12-06 13:26:41 +0300

    HuseyinOmer.c
    @themanoftalent
    Hüseyin Ömer Biricik

--- Odevler/name_surname.c
+++ Odevler/name_surname.c
@@ -1,1 +1,14 @@
 // Display your name and surname on screen 
+#include <stdio.h>	
+
+int main(){
+	
+	char name[20]="Huseyin Omer";
+	char surname[20]="Biricik";
+	
+	printf("my name is %s ",name);
+	printf("my surname is %s",surname);
+	
+	return 0;
+	
+}
